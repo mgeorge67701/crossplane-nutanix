f1851d595c604bfef96442eea9a3b0856d2bc9aa
Mohan George <georgmohan@gmail.com>
2025-08-29 17:05:50 +0100

    fix: Regenerate CRDs to fix YAML indentation

--- apis/v1beta1/providerconfig_types.go
+++ apis/v1beta1/providerconfig_types.go
@@ -71,7 +71,6 @@ type ProviderConfigSpec struct {
 
 // ProviderCredentials required to authenticate.
 type ProviderCredentials struct {
-	// Source of the provider credentials.
 	// +kubebuilder:validation:Enum=None;Secret;InjectedIdentity;Environment;Filesystem
 	Source xpv1.CredentialsSource `json:"source"`
 
