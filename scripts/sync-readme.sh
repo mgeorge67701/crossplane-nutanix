529f83217dc0ebbce10758ef2f54a6125d9c9bea
mohan.george <mohan.george@bottomline.com>
2025-11-06 11:29:07 +0000

    revert scripts

--- scripts/sync-readme.sh
+++ scripts/sync-readme.sh
@@ -2,14 +2,14 @@
 # Robustly sync README.md into crossplane.yaml meta.crossplane.io/readme annotation
 
 README_PATH="README.md"
-CROSSPLANE_YAML="package/crossplane.yaml"
+CROSSPLANE_YAML="crossplane-package/crossplane.yaml"
 
 if [ ! -f "$README_PATH" ]; then
   echo "README.md not found!"
   exit 1
 fi
 if [ ! -f "$CROSSPLANE_YAML" ]; then
-  echo "package/crossplane.yaml not found!"
+  echo "crossplane-package/crossplane.yaml not found!"
   exit 1
 fi
 
